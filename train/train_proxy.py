534eb9a5720e7e47e9a208b9db09bb17bd497ce5
rush-srik <rush.srik@gmail.com>
2026-07-31 11:45:58 -0400

    Add proper config and wandb logging to train_proxy

--- train/train_proxy.py
+++ train/train_proxy.py
@@ -1,14 +1,35 @@
+import torch
 from datasets import load_from_disk
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from trl import RewardTrainer, RewardConfig
 import random
 from utils.utils import apply_proxy_chat_template
+import wandb
 
 proxy_family_name = "EleutherAI/pythia"
 proxy_size = "14m"
 proxy_name = f"{proxy_family_name}-{proxy_size}"
 
+project_name = "overopt"
+
 val_ratio = 0.2
+lr = 5e-5
+micro_batch_size = 32
+effective_batch_size = 64
+
+assert effective_batch_size % micro_batch_size == 0
+grad_accum = effective_batch_size // micro_batch_size
+
+max_length = 2048
+eval_steps = 100
+save_steps = 100
+
 seed = 42
+
+tokenizer = AutoTokenizer.from_pretrained(proxy_name)
+tokenizer.add_special_tokens({"pad_token": "[PAD]"})
+model = AutoModelForSequenceClassification.from_pretrained(proxy_name, num_labels=1, dtype=torch.bfloat16)
+model.config.pad_token_id = tokenizer.pad_token_id
 
 rated_data = load_from_disk("data/datasets/train_proxy_rated")
 rated_data = rated_data.map(apply_proxy_chat_template)
@@ -21,19 +42,38 @@ val_data   = rated_data.filter(lambda ex: ex["prompt"] in val_prompts)
 train_data = rated_data.filter(lambda ex: ex["prompt"] not in val_prompts)
 
 args = RewardConfig(
+    # Training
+    learning_rate=lr,
+    per_device_train_batch_size=micro_batch_size,
+    gradient_accumulation_steps=grad_accum,
+
+    # Logging
     output_dir=f"runs/proxy/{proxy_size}",
-    max_length=2048,
-    eval_strategy="steps",
-    eval_steps=100,
     report_to=["wandb"],
     run_name=f"proxy-{proxy_size}",
+
+    # Misc
+    max_length=max_length,
+    eval_strategy="steps",
+    eval_steps=eval_steps,
+    save_steps=save_steps,
+    seed=seed,
 )
 
 trainer = RewardTrainer(
-    model=proxy_name,
+    model=model,
+    processing_class=tokenizer,
     args=args,
     train_dataset=train_data,
     eval_dataset=val_data
 )
 
+wandb.init(
+    project=project_name,
+    name=f"proxy-{proxy_size}",
+    group="proxy",
+    config={"lr": lr, "effective_batch": effective_batch_size}
+)
+
 trainer.train()
+trainer.save_model(f"models/proxy/{proxy_size}")
