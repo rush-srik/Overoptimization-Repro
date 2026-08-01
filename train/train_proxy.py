9631642c879495a66efdd2bec96bd4c09131d3c5
rush-srik <rush.srik@gmail.com>
2026-07-31 23:19:49 -0400

    Add command-line arguments to train_proxy

--- train/train_proxy.py
+++ train/train_proxy.py
@@ -1,21 +1,22 @@
-import torch
 from datasets import load_from_disk
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from trl import RewardTrainer, RewardConfig
 import random
 from utils.utils import apply_proxy_chat_template
 import wandb
+import sys
 
 proxy_family_name = "EleutherAI/pythia"
-proxy_size = "14m"
+proxy_size = sys.argv[1]
 proxy_name = f"{proxy_family_name}-{proxy_size}"
 
 project_name = "overopt"
 
 val_ratio = 0.2
 lr = 5e-5
-micro_batch_size = 32
+micro_batch_size = int(sys.argv[2])
 effective_batch_size = 64
+eval_batch_size=128
 
 assert effective_batch_size % micro_batch_size == 0
 grad_accum = effective_batch_size // micro_batch_size
@@ -28,7 +29,7 @@ seed = 42
 
 tokenizer = AutoTokenizer.from_pretrained(proxy_name)
 tokenizer.add_special_tokens({"pad_token": "[PAD]"})
-model = AutoModelForSequenceClassification.from_pretrained(proxy_name, num_labels=1, dtype=torch.bfloat16)
+model = AutoModelForSequenceClassification.from_pretrained(proxy_name, num_labels=1)
 model.config.pad_token_id = tokenizer.pad_token_id
 
 rated_data = load_from_disk("data/datasets/train_proxy_rated")
@@ -45,6 +46,7 @@ args = RewardConfig(
     # Training
     learning_rate=lr,
     per_device_train_batch_size=micro_batch_size,
+    per_device_eval_batch_size=eval_batch_size,
     gradient_accumulation_steps=grad_accum,
 
     # Logging
